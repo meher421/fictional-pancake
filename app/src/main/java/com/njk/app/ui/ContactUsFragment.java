8bd8e3525de950431533135c3572e862843366ae
meher <cholkerprasad@gmail.com>
2016-10-17 23:32:33 +0530

    - added contact us text

--- app/src/main/java/com/njk/app/ui/ContactUsFragment.java
+++ app/src/main/java/com/njk/app/ui/ContactUsFragment.java
@@ -5,17 +5,27 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.myapplication.R;
 
 public class ContactUsFragment extends Fragment {
 
+    private TextView mTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle(getString(R.string.tit_contact_us));
+
+
+        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        mTextView =(TextView) view.findViewById(R.id.contact_info_txt);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        mTextView.setText(R.string.contact_info_text);
+        return view;
     }
 
 
