c5afbede11df15faf8dae690c2226a1d2dac74e9
meher <cholkerprasad@gmail.com>
2016-10-23 17:01:21 +0530

    updated about us string

--- app/src/main/java/com/njk/app/ui/AboutUsFragment.java
+++ app/src/main/java/com/njk/app/ui/AboutUsFragment.java
@@ -2,9 +2,11 @@ package com.njk.app.ui;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.myapplication.R;
 
@@ -18,7 +20,11 @@ public class AboutUsFragment extends Fragment {
 
         getActivity().setTitle(getString(R.string.tit_about_us));
 
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.about_info_txt);
+        textView.setText(Html.fromHtml(getString(R.string.about_us)));
+
+        return view;
     }
 
 }
